4567a0dc20740829339dfca90f7a9d6c427fa720
kaushik Jain <83067256+Kaushikj14@users.noreply.github.com>
2024-03-21 13:01:55 +0530

    final commit

---
+++ doc.py
@@ -0,0 +1,35 @@
+from dotenv import load_dotenv
+# from langchain_community.embeddings import OpenAIEmbeddings
+from langchain_openai import OpenAIEmbeddings
+from langchain_openai import OpenAI
+import streamlit as st
+from langchain_community.document_loaders import DirectoryLoader
+from langchain.text_splitter import CharacterTextSplitter
+from langchain_community.vectorstores import Chroma
+# from langchain.chains import RetrievalQA
+from langchain.chains import RetrievalQA
+# from langchain_community.llms import OpenAI
+from langchain_openai import OpenAI
+# apikey = "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"
+
+
+st.title("Chatgpt to read Doc files")
+text = st.text_input("Enter question you want to ask")
+
+document_directory = "E://testfortext"
+loader = DirectoryLoader(document_directory)
+documents = loader.load()
+# print(documents)
+embeddings = OpenAIEmbeddings(openai_api_key="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")
+
+llm = OpenAI(openai_api_key="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")
+
+db = Chroma.from_documents(documents,embeddings)
+
+retriever = db.as_retriever()
+qa = RetrievalQA.from_chain_type(llm=llm,retriever=retriever)
+
+    
+if st.button("Submit and proceed"):
+    generated_text = qa(text)
+    st.success(str(generated_text['result']))
